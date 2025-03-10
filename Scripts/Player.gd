d9447a0543551f4a0dd8fd171fc35578a1239a26
Daniel H. <101878276+ITKillJoy@users.noreply.github.com>
2025-03-09 21:58:35 -0500

    changed player to use state machine

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,57 +1,25 @@
+class_name Player
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-var direction 
-var isAttacking = 0
 
-@onready var animated_sprite = $AnimatedSprite2D
+
+@onready var animations = $PlayerAnimations
+
+@onready var state_machine = $state_machine
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready() -> void:
+	state_machine.init(self)
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+func _unhandled_input(event: InputEvent) -> void:
+	state_machine.process_input(event)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_axis("Left", "Right")
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	selectAnimation()
-	
-	move_and_slide()
+func _physics_process(delta: float) -> void:
+	state_machine.process_physics(delta)
 
+func _process(delta: float) -> void:
+	state_machine.process_frame(delta)
 
-func selectAnimation():
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-	
-	if(isAttacking==0):
-		if is_on_floor():
-				animated_sprite.play("idle")
-		else:
-			animated_sprite.play("Jump")
-			
-		if Input.is_action_just_pressed("AttackA"):
-			isAttacking=1
-			animated_sprite.play("attack_1")
-		
-
-func _on_animated_sprite_2d_animation_finished():
-	if(animated_sprite.animation == "attack_1"):
-		isAttacking=0
---
+++ Scripts/StateMachines/player_move.gd
@@ -0,0 +1,30 @@
+extends State
+
+@export var jump_state: State
+@export var idle_state: State
+@export var fall_state: State
+
+func enter() -> void:
+	super()
+
+func process_input(event: InputEvent) -> State:
+	if Input.is_action_just_pressed('Jump') and parent.is_on_floor():
+		return jump_state
+	return null
+	
+func process_physics(delta: float) -> State:
+	parent.velocity.y += gravity * delta
+	
+	var movement = Input.get_axis('Left', 'Right') * move_speed
+	
+	if movement == 0 and !(Input.is_action_pressed("Left") or Input.is_action_pressed("Right")):
+		return idle_state
+	
+	parent.animations.flip_h = movement < 0
+	parent.velocity.x = movement
+	parent.move_and_slide()
+	
+	if !parent.is_on_floor():
+		return fall_state
+	
+	return null
